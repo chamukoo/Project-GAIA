15365a10913ce2bb6aae1d9de676812dfcb095d0
Mark Lhester Agellon <kianirel56@gmail.com>
2023-06-18 13:14:39 +0800

    Fix and add functionalities

--- src/pages/LearningCourse.tsx
+++ src/pages/LearningCourse.tsx
@@ -29,56 +29,7 @@ const LearningCourse: FunctionComponent = () => {
   }, [navigate]);
 
   return (
-    <MainLayout ownClass={styles.learningCourse}>
-      <div className={styles.footer}>
-        <div className={styles.contacts}>
-          <div className={styles.iconParent}>
-            <img className={styles.icon} alt='' src='/icon.svg' />
-            <div className={styles.gaiaplantcom}>
-              <p className={styles.p}>gaia@plant.com</p>
-            </div>
-          </div>
-          <div className={styles.frameParent}>
-            <img className={styles.icon} alt='' src='/frame4.svg' />
-            <div className={styles.div}>
-              <p className={styles.p}>+63 9327893355</p>
-            </div>
-          </div>
-          <div className={styles.layer1Parent}>
-            <img className={styles.icon} alt='' src='/layer-13.svg' />
-            <div className={styles.sampalocManila}>
-              <p className={styles.p}>Sampaloc Manila</p>
-            </div>
-          </div>
-        </div>
-        <div className={styles.socialMediIcons}>
-          <img className={styles.layer1Icon1} alt='' src='/layer-11.svg' />
-          <img className={styles.frameIcon1} alt='' src='/frame1.svg' />
-          <img className={styles.frameIcon1} alt='' src='/layer-12.svg' />
-        </div>
-        <div className={styles.others}>
-          <div
-            className={styles.privacyTerms}
-          >{`PRIVACY | TERMS AND CONDITIONS  `}</div>
-          <div className={styles.copyrightIconParent}>
-            <img
-              className={styles.copyrightIcon}
-              alt=''
-              src='/copyright-icon.svg'
-            />
-            <div className={styles.privacyTerms}>
-              2023 GAIA. All rights reserved
-            </div>
-          </div>
-        </div>
-        <img className={styles.logoIcon} alt='' src='/logo1@2x.png' />
-      </div>
-      <div
-        className={styles.logoComponent}
-        onClick={onLogoComponentContainerClick}
-      >
-        <img className={styles.logoIcon1} alt='' src='/logo@2x.png' />
-      </div>
+    <MainLayout footer={true} ownClass={styles.learningCourse}>
       <div className={styles.takeCourseFrame}>
         <div className={styles.header}>
           <b>{`PLANT `}</b>
